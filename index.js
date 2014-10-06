34bee10c05b0ddfd0b615575dc16b9d60676091e
James Friend <james@jsdf.co>
2014-10-06 08:15:38 +0800

    fix for strict mode

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 module.exports = function deepFreeze (o) {
   Object.freeze(o);
 
+  var oIsFunction = typeof o === "function";
+
   Object.getOwnPropertyNames(o).forEach(function (prop) {
     if (o.hasOwnProperty(prop)
+    && (oIsFunction ? prop !== 'caller' && prop !== 'callee' && prop !== 'arguments' : true )
     && o[prop] !== null
     && (typeof o[prop] === "object" || typeof o[prop] === "function")
     && !Object.isFrozen(o[prop])) {
